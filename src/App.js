4e20ae8aaaed724358c7eb019c7d81f899818ceb
Hector Garcia Rosales <ghectormus@gmail.com>
2023-12-25 15:29:22 -0600

    add: structure for main div

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import freeCodeCampLogo from "./imagenes/freecodecamp-logo.png"
 
 function App() {
   return (
     <div className="App">
-      Hola Mandi
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          src={freeCodeCampLogo}
+          className="freecodecamp-logo"
+          alt="Logo de FreeCodeCamp" />
+      </div>
 
     </div>
   );
