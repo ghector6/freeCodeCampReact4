e463d1551333942f225dd0265b1cf38161ab51f2
Hector Garcia Rosales <ghectormus@gmail.com>
2023-12-25 17:16:44 -0600

    add: second class based on fn
    using a ternary operator to define if the class is an operator depending on written fn esOperador()

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 function Boton(props) {
+   const esOperador = valor => {
+     return isNaN(valor) && (valor != ".") && (valor != "=");
+  }
   return(
-    <div>
+    <div className={`boton-contenedor ${esOperador(props.children) ? "operador" : null}`}>
       {props.children}
     </div>
   )
